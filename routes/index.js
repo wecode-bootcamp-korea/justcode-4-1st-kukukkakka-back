6ad6e9f2ec07372af978078936283b73a834b840
xxziiko <xxziiko@gmail.com>
2022-03-28 22:07:27 +0900

    Modify: kkukku_kkakka backend 초기세팅

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const router = express.Router(); // express 라우팅 기능을 사용하기 위해서 router 객체가 필요합니다.
 
-router.use("users", userRoute);
-router.use("products", productRoute);
-router.use("/orders", orderRoute);
 
 module.exports = router; // 이렇게 내보낸 router 는 express app 의 미들웨어로 사용됩니다.
