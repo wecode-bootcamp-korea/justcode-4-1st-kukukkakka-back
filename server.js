ccc20c26311b0a90fcb5abbcda5a51704a61cc8c
xxziiko <xxziiko@gmail.com>
2022-03-31 14:55:47 +0900

    [Pull]: products api pull

--- server.js
+++ server.js
@@ -16,12 +16,11 @@ app.get("/ping", (req, res) => {
 
 const server = http.createServer(app);
 dotenv.config();
-// const PORT = process.env.PORT;
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const start = async () => {
   try {
-    server.listen(8000, () => console.log(`Server is listening on ${PORT}`));
+    server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
   } catch (err) {
     console.error(err);
     await prisma.$disconnect();
