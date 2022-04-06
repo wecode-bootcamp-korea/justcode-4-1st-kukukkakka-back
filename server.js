91ae88152eaab349c1ab1c92cc792d553b399a7e
songyi-p <97720036+songyi-p@users.noreply.github.com>
2022-04-06 14:46:53 +0900

    Revert "[장바구니] post api, get api 1차 완료 / 미들웨어 함수 추가"

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const http = require("http");
 const express = require("express");
-const cors = require("cors");
-const { PrismaClient } = require("@prisma/client");
+// const { PrismaClient } = require("@prisma/client");
 const routes = require("./routes");
 const dotenv = require("dotenv");
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 const app = express();
 
-app.use(cors());
 app.use(express.json());
-
 app.use(routes);
+
 app.get("/ping", (req, res) => {
   res.json({ message: "pong" });
 });
