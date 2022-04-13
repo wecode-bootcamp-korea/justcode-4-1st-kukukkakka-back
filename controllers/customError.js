fb2aeea28b334e43804b6ca965e0fe98cc16f156
dami kim <tmkim@sunnywt.com>
2022-04-13 15:42:26 +0900

    [리팩토링] signup key error 변경

--- controllers/customError.js
+++ controllers/customError.js
@@ -1,19 +1,34 @@
-//클래스로 만들기
-class userInputError extends Error {
-  constructor(key) {
-    super(key);
-    const ErrorMapping = {
-      'email': {
-        message: "KEY ERROR : email",
-        statusCode: 400,
-      },
-      'password': {
-        message: "KEY ERROR : password",
-        statusCode: 400,
-      },
-    }
-    this.err.statusCode = this.ErrorMapping.statusCode
-    this.err.message = this.ErrorMapping[key].message
-  }
-}
+// // const { Error } = require('./userController')
 
+// class userInputError extends Error {
+//   constructor(key) {
+//     super(key);
+//     console.log('여기까지 오지도 못함')
+//     const ErrorMapping = {
+//       email: {
+//         message: "KEY ERROR : email",
+//         statusCode: 400,
+//       },
+//       password: {
+//         message: "KEY ERROR : password",
+//         statusCode: 400,
+//       },
+//       username: {
+//         message: "KEY ERROR : username",
+//         statusCode: 400,
+//       },
+//       genderId: {
+//         message: "KEY ERROR : genderId",
+//         statusCode: 400,
+//       },
+//       policyAgreed: {
+//         message: "KEY ERROR : policyAgreed",
+//         statusCode: 400,
+//       },
+//     };
+//     userInputError.statusCode = ErrorMapping[key].statusCode;
+//     userInputError.message = ErrorMapping[key].message;
+//     throw userInputError;
+//   }
+// }
+// module.exports = { userInputError };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const userService = require("../services/userService");
-// const userInputError = require("./customError");
+
 class userInputError extends Error {
   constructor(key) {
     console.log("여기까지 오긴 하냐?", key);
@@ -26,11 +26,8 @@ class userInputError extends Error {
         statusCode: 400,
       },
     };
-    // console.log("여기까지도 설마 오냐?", ErrorMapping[key]);
     userInputError.statusCode = ErrorMapping[key].statusCode;
-    // console.log("너는 어떻게 나오고 있니 ", userInputError.statusCode);
     userInputError.message = ErrorMapping[key].message;
-    // console.log("너는 어떻게 나오고 있니2 ", userInputError.message);
     throw userInputError
   }
 }
@@ -38,55 +35,16 @@ class userInputError extends Error {
 const signup = async (req, res) => {
   try {
     const { email, password, username, policyAgreed, genderId } = req.body;
-
-    //for 문을 이용하여 정리
-    const REQUIRED_KEYS = { email, password, username, policyAgreed, genderId };
-
-    // for (const key in REQUIRED_KEYS) {
-    //   if (!REQUIRED_KEYS[key]) {
-    //     const error = new Error(`KEY_ERROR : ${key}`);
-    //     error.statusCode = 400;
-    //     throw error;
-    //   }
-    // }
-
+    const REQUIRED_KEYS = { email, password, username, policyAgreed, genderId };   //for 문을 이용하여 정리
     for (const key in REQUIRED_KEYS) {
       if (!REQUIRED_KEYS[key]) {
+        const error = new Error()
         throw new userInputError(key);
       }
     }
 
-    // if (!email) {
-    //   const error = new Error("KEY ERROR : NO_EMAIL");
-    //   error.statusCode = 400;
-    //   throw error;
-    // }
 
-    // if (!password) {
-    //   const error = new Error("KEY ERROR : NO_PASSWORD");
-    //   error.statusCode = 400;
-    //   throw error;
-    // }
-
-    // if (!username) {
-    //   const error = new Error("KEY ERROR : NO_USERNAME");
-    //   error.statusCode = 400;
-    //   throw error;
-    // }
-
-    // if (!policyAgreed) {
-    //   const error = new Error("KEY ERROR : NO_POLICY_AGREED");
-    //   error.statusCode = 400;
-    //   throw error;
-    // }
-
-    // if (!genderId) {
-    //   const error = new Error("KEY ERROR : NO_GENDER_ID");
-    //   error.statusCode = 400;
-    //   throw error;
-    // }
-
-    const createUser = await userService.signup(
+    await userService.signup(
       email,
       password,
       username,
@@ -96,8 +54,6 @@ const signup = async (req, res) => {
 
     return res.status(201).json({ message: "SIGNUP_SUCCESS" });
   } catch (err) {
-    console.log(err.status);
-    console.log(err.message);
     return res.status(err.statusCode || 500).json({ message: err.message });
   }
 };
@@ -149,4 +105,4 @@ const getUserName = async (req, res) => {
   }
 };
 
-module.exports = { signup, login, duplicateCheck, getUserName };
+module.exports = { signup, login, duplicateCheck, getUserName, Error };
